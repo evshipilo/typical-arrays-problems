0791a7c26e239d5df8d6483cc1c3e096a033b787
Yauheni Shypila <evshipilo@gmail.com>
2020-02-24 16:54:38 +0300

    fix: pass test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+exports.min = function min(array) {
+    if (!array || array.length == 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    return array.sort((a, b) => a - b)[0];
 
 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length == 0) return 0;
+    return array.sort((a, b) => b - a)[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) return 0;
+    return array.reduce((sum, current) => sum + current, 0)/array.length;
 }
